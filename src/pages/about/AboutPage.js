dfeaf7e7dc463c63005c8166e2e7e9b01801e107
EndYou <jorgesenjudo@gmail.com>
2022-12-23 02:34:09 -0500

    avanzando en el diseno

--- src/pages/about/AboutPage.js
+++ src/pages/about/AboutPage.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 export const AboutPage = () => {
+
+    const lastLang = localStorage.getItem('lastLang')||'en'
+    
     return (
-        <h1>About</h1>
+        <head>
+            <title> About | Jorge Senjudo Portafolio de Desarrollo</title>
+            <meta
+            lang={(lastLang)}
+            name="description"
+            content="Information about the developer experience and education of developer Anthony Fernandez."
+          />
+        </head>
     )
 }
