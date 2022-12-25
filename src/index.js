7a5ddafc454b70e843670823c22996e0129e6b15
EndYou <jorgesenjudo@gmail.com>
2022-12-25 13:44:00 -0500

    Formateando Codigo

--- src/index.js
+++ src/index.js
@@ -2,29 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AppRouter } from './routes/AppRouter';
 import './styles/styles.scss'
-import {I18nextProvider} from 'react-i18next'
+import { I18nextProvider } from 'react-i18next'
 import i18next from 'i18next'
 
 import global_es from './translations/ES/global.json'
 import global_en from './translations/EN/global.json'
 
-const lastLang = localStorage.getItem('lastLang')||'en'
+const lastLang = localStorage.getItem('lastLang') || 'en'
 var lang
 
-if(lastLang === 'ES'){
-    lang = 'es' 
-}else{
+if (lastLang === 'ES') {
+    lang = 'es'
+} else {
     lang = 'en'
 }
 
 i18next.init({
-    interpolation: {escapeValue: false},
+    interpolation: { escapeValue: false },
     lng: lang,
-    resources:{
-        es:{
+    resources: {
+        es: {
             global: global_es
         },
-        en:{
+        en: {
             global: global_en
         }
     }
@@ -33,7 +33,7 @@ i18next.init({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <I18nextProvider i18n={i18next}>
-        <AppRouter/>
+        <AppRouter />
     </I18nextProvider>
 );
 
