40f699d336c1fbe2efbf14910c585de49b26b332
EndYou01 <jorgesenjudo@gmail.com>
2023-04-10 11:48:22 +0200

    Aplicando useEffect en ImageLoaded

--- src/components/imageLoaded/ImageLoaded.js
+++ src/components/imageLoaded/ImageLoaded.js
@@ -1,19 +1,29 @@
 
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import { Spinner } from './Spinner'
 
 export const ImageLoaded = ({ effect, classname, alt, src }) => {
 
     let [imageLoaded, setImageLoaded] = useState(false)
+    
+    useEffect(() => {
+        // Read the state from local storage when the component is mounted
+        let localImageLoaded = localStorage.getItem("imageLoaded")
+        setImageLoaded(localImageLoaded)
+    }, [])
 
+    useEffect(() => {
+        // Save the state to local storage after the first rendering
+        localStorage.setItem("imageLoaded", imageLoaded)
+    })
     return (
         <div
             className={(classname === "bg_image") ? "ImageLoadedBackGround" : ""}
         >
-            {!imageLoaded && <Spinner/>}
-            
+            {!imageLoaded && <Spinner />}
+
             <LazyLoadImage
                 effect={effect}
                 className={(classname === "bg_image") ? "bg_imageImageLoaded" : classname}
