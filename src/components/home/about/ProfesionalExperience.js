b473496941d914c58867b6a8b5d36edc5ef6e1c6
EndYou01 <jorgesenjudo@gmail.com>
2024-01-27 00:32:50 -0500

    Update about work experience and translation.

--- src/components/home/about/ProfesionalExperience.js
+++ src/components/home/about/ProfesionalExperience.js
@@ -63,7 +63,7 @@ const ProfesionalExperience = () => {
 
 
                     <div style={{ border: "none" }} className={"tr"}>
-                        <div className={"first_td"}>2023/07 - {text("general.present")}</div>
+                        <div className={"first_td"}>2023/06 - 2023/12</div>
 
                         <div className={fillVariable ? "second_td" : "second_td_dark"}>
                             <h3>{text("general.FrontendWebDeveloper")}</h3>
@@ -86,6 +86,42 @@ const ProfesionalExperience = () => {
                                 </li>
                                 <li>
                                     {text("about.worksInfo.text8")}
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+
+                    <div style={{ border: "none" }} className={"tr"}>
+                        <div className={"first_td"}>2023/12 - {text("general.present")}</div>
+
+                        <div className={fillVariable ? "second_td" : "second_td_dark"}>
+                            <h3>{text("general.ChiefoftheWebDepartment")}</h3>
+                            <div className='second_td_empressContainer'>
+                                <a className='secundary_color' href="https://www.tecopos.com/">TecoPos Technology Company, La Habana</a>
+                                <img className='second_td_empressIMG' src="logos/TecoposLogo2.jpg" alt="..." />
+                            </div>
+                            <h4>{text("general.KeyResponsibilities")}.</h4>
+                            <ul>
+                                <li>
+                                    {text("about.worksInfo.text9")}
+                                    {/* <ul>
+                                        <li>
+                                            {text("about.worksInfo.text6")}
+                                        </li>
+                                    </ul> */}
+                                </li>
+                                <li>
+                                    {text("about.worksInfo.text10")}
+                                </li>
+                                <li>
+                                    {text("about.worksInfo.text11")}
+                                </li>
+                                <li>
+                                    {text("about.worksInfo.text12")}
+                                </li>
+                                <li>
+                                    {text("about.worksInfo.text13")}
                                 </li>
                             </ul>
                         </div>
