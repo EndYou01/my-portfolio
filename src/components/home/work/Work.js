141d97045595c93b26c89257078f82773a25f12c
Jorge Senjudo <95231207+EndYou01@users.noreply.github.com>
2025-01-06 11:55:07 -0500

    Agregando proyectos

--- src/components/home/work/Work.js
+++ src/components/home/work/Work.js
@@ -1,291 +1,72 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import { Fade } from "react-awesome-reveal";
+import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Fade } from 'react-awesome-reveal';
 import { LearnMoreButton } from '../../learnMoreButton/LearnMoreButton';
-import { useContext } from 'react';
 import { FillContext } from '../../../context/FillContext';
 import { ImageLoaded } from '../../imageLoaded/ImageLoaded';
 import { Link, useLocation } from 'react-router-dom';
 
+const ProjectSection = ({ title, description, bgImage, logoImage, altText, buttonPath }) => (
+  <section className={`projects_section ${!useContext(FillContext).fillVariable && 'projects_section_dark'}`}>
+    <Fade>
+      <div className='project_container'>
+        <ImageLoaded effect="blur" classname="bg_image" alt={`${altText} Background`} src={bgImage} />
+        <div className='project_image_container'>
+          <ImageLoaded effect="blur" classname="project_image" alt={`${altText} Logo`} src={logoImage} />
+        </div>
+        <div className='project_info_container'>
+          <h2>{title}</h2>
+          <p>{description}</p>
+          <LearnMoreButton pathname={buttonPath} />
+        </div>
+      </div>
+    </Fade>
+  </section>
+);
+
 export const Work = () => {
-	const [text] = useTranslation("global")
+  const { t } = useTranslation('global');
+  const isWorkLocation = useLocation().pathname.includes('work');
 
-	const { fillVariable } = useContext(FillContext)
+  const projects = [
+	{ title: 'SinTerceros', description: t('sinterceros.p1'), bgImage: 'freelance/sinterceros/st1.png', logoImage: 'logos/sintercerosLogo.png', buttonPath: '/sinterceros', altText: 'SinTerceros' },
+	{ title: 'El Gelato', description: t('sinterceros.p1'), bgImage: 'freelance/gelato/gelato1.png', logoImage: 'logos/gelatologo.jpg', buttonPath: '/sinterceros', altText: 'Gelato' },
+	{ title: 'Tecopos Administracion', description: t('sinterceros.p1'), bgImage: 'freelance/tecopos/tecopos1.png', logoImage: 'logos/TecoposLogo.png', buttonPath: '/sinterceros', altText: 'TecoposAdmin' },
+    ...(isWorkLocation
+      ? [
+		{ title: 'Lyn-Arte-Gráfico', description: t('lyn.p1'), bgImage: 'freelance/lyn/lyn1.png', logoImage: 'logos/lynLogo.png', buttonPath: '/lyn', altText: 'Lyn-Arte-Gráfico' },
+			{ title: t('tmf.h2'), description: t('tmf.p1'), bgImage: 'tmf/tmf1.png', logoImage: 'logos/tmf_logo.png', buttonPath: '/mandalorian', altText: 'TMF' },
+			{ title: 'SNB 2022', description: t('snb.p1'), bgImage: 'snb2022/snb_1.jpg', logoImage: 'logos/snb_logo1x5.png', buttonPath: '/snb', altText: 'SNB 2022' },
+	{ title: 'Celebren Web', description: t('sinterceros.p1'), bgImage: 'freelance/celebren/celebren1.png', logoImage: 'logos/celebrenlogo.png', buttonPath: '/sinterceros', altText: 'CelebrenWeb' },
+          { title: "Kids'R'Us", description: t('kidsrUs.p1'), bgImage: 'freelance/kidsrUs/KidsrUs_image.png', logoImage: 'logos/kidsrUs_logo.png', buttonPath: '/kidsrUs', altText: "Kids'R'Us" },
+		  { title: 'Cross Border Trade', description: t('crossBorder.p1'), bgImage: 'freelance/crossBorderTrade/crossBorder.jpg', logoImage: 'logos/crossBorder_logo.png', buttonPath: '/crossBorder', altText: 'Cross Border Trade' },
+          { title: 'Confort', description: t('confort.p'), bgImage: 'freelance/confort/Pantalla_Principal.jpg', logoImage: 'logos/confort_logo.png', buttonPath: '/confort', altText: 'Confort' },
+		  { title: 'Coremant', description: t('coremant.p'), bgImage: 'freelance/coremant/coremant.png', logoImage: 'logos/coremant_logo.png', buttonPath: '/coremant', altText: 'Coremant' },
+		  { title: 'CitasApp', description: t('citas.p1'), bgImage: 'freelance/citasApp/citas1.png', logoImage: 'logos/citas_logo.png', buttonPath: '/citasApp', altText: 'CitasApp' },
+        ]
+      : []),
+  ];
 
-	const location = useLocation().pathname.includes('work')
-
-	return (
-		<div id="work" className='work_container'>
-
-			<Fade>
-				<h1 className='header'>{text("work.h1")}</h1>
-			</Fade>
-
-			<div>
-				<section className={
-					(fillVariable)
-						? 'projects_section'
-						: 'projects_section projects_section_dark'
-				}>
-					<Fade>
-						<div className='project_container'>
-
-							<ImageLoaded effect={""} classname={"bg_image"} alt={"TMF Image"} src={"tmf/tmf1.png"} />
-
-							<div className='project_image_container'>
-
-								<ImageLoaded effect={"blur"} classname={"project_image"} alt={"TMF Logo"} src={"logos/tmf_logo.png"} />
-
-							</div>
-
-							<div className='project_info_container'>
-								<h2>{text("tmf.h2")}</h2>
-								<p>
-									{text("tmf.p1")}
-								</p>
-								<LearnMoreButton pathname='/mandalorian' />
-							</div>
-						</div>
-					</Fade>
-				</section>
-
-				<br />
-
-				<section className={
-					(fillVariable)
-						? 'projects_section'
-						: 'projects_section projects_section_dark'
-				}>
-					<Fade>
-						<div className='project_container'>
-
-							<ImageLoaded effect={""} classname={"bg_image"} alt={"SNB 2022 Image"} src={"snb2022/snb_1.jpg"} />
-
-
-							<div className='project_image_container'>
-
-								<ImageLoaded effect={"blur"} classname={"project_image"} alt={"Basketball Center Logo"} src={"logos/snb_logo1x5.png"} />
-
-							</div>
-
-							<div className='project_info_container'>
-								<h2>SNB 2022</h2>
-								<p>{text("snb.p1")}
-								</p>
-								<LearnMoreButton pathname='/snb' />
-							</div>
-						</div>
-					</Fade>
-				</section>
-
-				<br />
-
-				<Fade>
-					<h1 className='header'>{text("work.freelance")}</h1>
-				</Fade>
-
-				<section className={
-					(fillVariable)
-						? 'projects_section'
-						: 'projects_section projects_section_dark'
-				}>
-					<Fade>
-						<div className='project_container'>
-
-							<ImageLoaded effect={"blur"} classname={"bg_image"} alt={"CitasApp Image"} src={"freelance/citasApp/citas1.png"} />
-
-							<div className='project_image_container'>
-
-								<ImageLoaded effect={"blur"} classname={"project_image"} alt={"CitasApp Logo"} src={"logos/citas_logo.png"} />
-
-							</div>
-							<div className='project_info_container'>
-								<h2>CitasApp</h2>
-								<p>{text("citas.p1")}
-								</p>
-								<LearnMoreButton pathname='/citasApp' />
-							</div>
-						</div>
-					</Fade>
-				</section>
-
-				{
-					!location && (
-						<>
-							<br />
-							<br />
-							<Link to={"/work"} className='seeMore'>{text("general.seeMore")}</Link>
-						</>
-					)
-				}
-
-				<br />
-				{
-					location && (
-						<>
-
-							<section className={
-								(fillVariable)
-									? 'projects_section'
-									: 'projects_section projects_section_dark'
-							}>
-								<Fade>
-									<div className='project_container'>
-										<ImageLoaded effect={"blur"} classname={"bg_image"} alt={"Confort Image"} src={"freelance/confort/Pantalla_Principal.jpg"} />
-
-
-
-										<div className='project_image_container'>
-
-											<ImageLoaded effect={"blur"} classname={"project_image"} alt={"Confort Logo"} src={"logos/confort_logo.png"} />
-
-										</div>
-										<div className='project_info_container'>
-											<h2>Confort</h2>
-											<p>{text("confort.p")}
-											</p>
-											<LearnMoreButton pathname='/confort' />
-										</div>
-									</div>
-								</Fade>
-							</section>
-
-							<br />
-
-							<section className={
-								(fillVariable)
-									? 'projects_section'
-									: 'projects_section projects_section_dark'
-							}>
-								<Fade>
-									<div className='project_container'>
-										<ImageLoaded effect={"blur"} classname={"bg_image"} alt={"Coremant Image"} src={"freelance/coremant/coremant.png"} />
-
-
-										<div className='project_image_container'>
-
-											<ImageLoaded effect={"blur"} classname={"project_image"} alt={"Coremant Logo"} src={"logos/coremant_logo.png"} />
-
-										</div>
-										<div className='project_info_container'>
-											<h2>Coremant</h2>
-											<p>{text("coremant.p")}
-											</p>
-											<LearnMoreButton pathname='/coremant' />
-										</div>
-									</div>
-								</Fade>
-							</section>
-
-							<br />
-
-							<section className={
-								(fillVariable)
-									? 'projects_section'
-									: 'projects_section projects_section_dark'
-							}>
-								<Fade>
-									<div className='project_container'>
-										<ImageLoaded effect={"blur"} classname={"bg_image"} alt={"CrossBorderTrade Image"} src={"freelance/crossBorderTrade/crossBorder.jpg"} />
-
-										<div className='project_image_container'>
-
-											<ImageLoaded effect={"blur"} classname={"project_image"} alt={"CrossBorderTrade Logo"} src={"logos/crossBorder_logo.png"} />
-
-										</div>
-										<div className='project_info_container'>
-											<h2>Cross Border Trade</h2>
-											<p>{text("crossBorder.p1")}
-											</p>
-											<LearnMoreButton pathname='/crossBorder' />
-										</div>
-									</div>
-								</Fade>
-							</section>
-
-							<br />
-
-							<section className={
-								(fillVariable)
-									? 'projects_section'
-									: 'projects_section projects_section_dark'
-							}>
-								<Fade>
-									<div className='project_container'>
-										<ImageLoaded effect={"blur"} classname={"bg_image"} alt={"CrossBorderTrade Image"} src={"freelance/kidsrUs/KidsrUs_image.png"} />
-
-										<div className='project_image_container'>
-
-											<ImageLoaded effect={"blur"} classname={"project_image"} alt={"KidsrUs Logo"} src={"logos/kidsrUs_logo.png"} />
-
-										</div>
-										<div className='project_info_container'>
-											<h2>Kids'R'Us</h2>
-											<p>{text("kidsrUs.p1")}
-											</p>
-											<LearnMoreButton pathname='/kidsrUs' />
-										</div>
-									</div>
-								</Fade>
-							</section>
-							<br />
-
-							<section className={
-								(fillVariable)
-									? 'projects_section'
-									: 'projects_section projects_section_dark'
-							}>
-								<Fade>
-									<div className='project_container'>
-										<ImageLoaded effect={"blur"} classname={"bg_image"} alt={"Lyn-Arte-Gráfico Image"} src={"freelance/lyn/lyn1.png"} />
-
-										<div className='project_image_container'>
-
-											<ImageLoaded effect={"blur"} classname={"project_image"} alt={"Lyn-Arte-Gráfico Logo"} src={"logos/lynLogo.png"} />
-
-										</div>
-										<div className='project_info_container'>
-											<h2>Lyn-Arte-Gráfico</h2>
-											<p>{text("lyn.p1")}
-											</p>
-											<LearnMoreButton pathname='/lyn' />
-										</div>
-									</div>
-								</Fade>
-							</section>
-							<br />
-
-
-							<section className={
-								(fillVariable)
-									? 'projects_section'
-									: 'projects_section projects_section_dark'
-							}>
-								<Fade>
-									<div className='project_container'>
-										<ImageLoaded effect={"blur"} classname={"bg_image"} alt={"sinterceros Image"} src={"freelance/sinterceros/st1.png"} />
-
-										<div className='project_image_container'>
-
-											<ImageLoaded effect={"blur"} classname={"project_image"} alt={"sinterceros Logo"} src={"logos/sintercerosLogo.png"} />
-
-										</div>
-										<div className='project_info_container'>
-											<h2>SinTerceros</h2>
-											<p>{text("sinterceros.p1")}
-											</p>
-											<LearnMoreButton pathname='/sinterceros' />
-										</div>
-									</div>
-								</Fade>
-							</section>
-							<br />
-
-						</>
-					)
-				}
-			</div>
-
-		</div>
-	)
-}
+  return (
+    <div id="work" className="work_container">
+      <Fade>
+        <h1 className="header">{t('work.h1')}</h1>
+      </Fade>
+      {projects.map((project, index) => (
+        <React.Fragment key={index}>
+          <ProjectSection {...project} />
+          <br />
+        </React.Fragment>
+      ))}
+      {!isWorkLocation && (
+        <>
+          <br />
+          <br />
+          <Link to="/work" className="seeMore">
+            {t('general.seeMore')}
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
